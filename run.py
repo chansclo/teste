0c98f7a285ed9d58d709a88518e56f9f362e14df
João Baiochi <jao.baiochi@gmail.com>
2015-10-21 00:47:42 -0200

    Batata legal

--- run.py
+++ run.py
@@ -6,9 +6,11 @@ def add(x,y): return x + y
 
 def subtract(x,y): return x - y
 
-def multiply(x, y): return x * y
+def multiply(x,y): return x * y
 
-def divide(x, y): return x / y
+def divide(x,y): return x / y
+
+def factorial(x,y): return x ** y 
 
 # take input from the user
 #print("Select operation.")
